de668c53eb5dfa2a669a34f546c3e3587b4a09cb
init0-lux <init0.lux@protonmail.com>
2026-06-02 02:28:17 +0530

    refactor: rewrite keil-c51 as proper derivation with requireFile;

--- pkgs/keil-c51.nix
+++ pkgs/keil-c51.nix
@@ -1,26 +1,83 @@
 {
-  aria2,
+  lib,
+  requireFile,
+  stdenv,
+  which,
   wineWow64Packages,
-  writeShellScriptBin,
 }:
 let
-  url = "https://www.keil.com/fid/yzg04kwtdy9j1wwcox91rgd4umlvhujmp9yxd1/files/eval/c51v961.exe";
+  version = "9.61";
 in
-writeShellScriptBin "keil-c51" ''
-  export WINEPREFIX="''${WINEPREFIX:-$HOME/.keil_prefix}"
-  export WINEDLLOVERRIDES="mscoree,mshtml="
+stdenv.mkDerivation {
+  pname = "keil-c51";
+  inherit version;
 
-  LOCAL_INSTALLER="$(pwd)/c51v961.exe"
-  TMP_INSTALLER="/tmp/c51v961.exe"
+  installer = requireFile {
+    name = "c51v961.exe";
+    url = "https://www.keil.com/demo/eval/c51.htm";
+    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+  };
 
-  if [ -f "$LOCAL_INSTALLER" ]; then
-    INSTALLER="$LOCAL_INSTALLER"
-  elif [ -f "$TMP_INSTALLER" ]; then
-    INSTALLER="$TMP_INSTALLER"
-  else
-    ${aria2}/bin/aria2c -x 16 -s 16 -k 1M -o c51v961.exe -d /tmp "${url}"
-    INSTALLER="$TMP_INSTALLER"
-  fi
+  nativeBuildInputs = [
+    which
+    wineWow64Packages.stable
+  ];
 
-  ${wineWow64Packages.stable}/bin/wine "$INSTALLER"
-''
+  dontUnpack = true;
+
+  buildPhase = ''
+    runHook preBuild
+
+    export WINEPREFIX="$PWD/.wine"
+    export WINEDLLOVERRIDES="mscoree,mshtml="
+    export WINEARCH="win32"
+
+    wineboot --init
+    ${wineWow64Packages.stable}/bin/wine "$installer" /quiet
+
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    target="$out/opt/keil-c51"
+    mkdir -p "$target"
+
+    cp -r "$WINEPREFIX/drive_c/Keil" "$target/"
+    mkdir -p "$out/bin"
+
+    cat > "$out/bin/keil-c51" <<'WRAPPER'
+    #!/bin/sh
+    export WINEPREFIX="''${WINEPREFIX:-$HOME/.keil_prefix}"
+    export WINEDLLOVERRIDES="mscoree,mshtml="
+    if [ ! -d "$WINEPREFIX/drive_c/Keil" ]; then
+      mkdir -p "$(dirname "$WINEPREFIX/drive_c/Keil")"
+      cp -r @out@/opt/keil-c51/Keil "$WINEPREFIX/drive_c/Keil"
+    fi
+    exec ${wineWow64Packages.stable}/bin/wine "$installer" "$@"
+    WRAPPER
+
+    substituteInPlace "$out/bin/keil-c51" \
+      --subst-var-by out "$out"
+
+    chmod +x "$out/bin/keil-c51"
+
+    runHook postInstall
+  '';
+
+  preferLocalBuild = true;
+
+  meta = with lib; {
+    description = "Keil C51 compiler for 8051 embedded development";
+    homepage = "https://www.keil.com/demo/eval/c51.htm";
+    license = licenses.unfree;
+    platforms = [
+      "x86_64-linux"
+      "i686-linux"
+    ];
+    maintainers = [ ];
+    sourceProvenance = with sourceTypes; [ binaryNativeCode ];
+    mainProgram = "keil-c51";
+  };
+}
