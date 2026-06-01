5abb784c9650dd7071c5c14a3e38b6e0ba145533
init0-lux <init0.lux@protonmail.com>
2026-06-02 02:54:22 +0530

    refactor: replace buildFHSUserEnv with buildFHSEnv; add meta to all packages;

--- pkgs/keil-uvision-fhs.nix
+++ pkgs/keil-uvision-fhs.nix
@@ -1,12 +1,26 @@
 {
-  buildFHSUserEnv,
+  buildFHSEnv,
   callPackage,
+  lib,
 }:
 let
   keil-uvision = callPackage ./keil-uvision.nix { };
 in
-buildFHSUserEnv {
+buildFHSEnv {
   name = "keil-uvision-fhs";
   targetPkgs = pkgs: [ keil-uvision ];
   runScript = "keil-uvision";
+
+  meta = with lib; {
+    description = "Keil MDK µVision 5 IDE in FHS-compatible environment";
+    homepage = "https://www.keil.com/demo/eval/arm.htm";
+    license = licenses.unfree;
+    platforms = [
+      "x86_64-linux"
+      "i686-linux"
+    ];
+    maintainers = [ ];
+    sourceProvenance = with sourceTypes; [ binaryNativeCode ];
+    mainProgram = "keil-uvision-fhs";
+  };
 }
